76df583e9a22dc981c1347e84e14e9c6c7413b83
Marcin Dziewulski <hello@mobily.pl>
2020-03-27 23:00:13 +0100

    Fix withConverter types

--- src/withConverter.ts
+++ src/withConverter.ts
@@ -2,7 +2,7 @@ import {
   DocumentReference,
   CollectionReference,
   Query,
-  FirestoreDataConverter,
+  DocumentData,
 } from '@firebase/firestore-types'
 
 import { curry2 } from './internal/curry2'
@@ -10,15 +10,18 @@ import { ExtractData } from './internal/types'
 
 type Acc = DocumentReference | CollectionReference | Query
 
+export type DataConverter<T> = {
+  fromFirestore: (data: T) => T
+  toFirestore: (data: T) => DocumentData
+}
+
 type Curry2 = {
-  <T extends Acc>(converter: FirestoreDataConverter<ExtractData<T>>): (
-    acc: T,
-  ) => T
-  <T>(converter: FirestoreDataConverter<ExtractData<T>>, acc: T): T
+  <T extends Acc>(converter: DataConverter<ExtractData<T>>): (acc: T) => T
+  <T>(converter: DataConverter<ExtractData<T>>, acc: T): T
 }
 
 export const withConverter: Curry2 = curry2(
-  (converter: FirestoreDataConverter<any>, acc: any): any => {
+  (converter: DataConverter<any>, acc: any): any => {
     return acc.withConverter(converter)
   },
 )
