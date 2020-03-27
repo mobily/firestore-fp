b1b318510e09604fe9bfdde2fd5c02af9c0e6b69
Marcin Dziewulski <hello@mobily.pl>
2020-03-27 22:07:48 +0100

    Revert "Update package script"
    This reverts commit 117f7d84469f5060e5bae2f9750dbcb621cdadb8.

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,6 +2,8 @@
 
 yarn clean
 yarn build
+yarn version --no-git-tag-version --no-commit-hooks
 cp README.md dist
 cp package.json dist
+git stash save --keep-index --include-untracked
 # np --yolo --no-yarn --contents=dist
