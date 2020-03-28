8cfa73e79547e51d055d0035d312337721ddda34
Marcin Dziewulski <hello@mobily.pl>
2020-03-28 16:08:50 +0100

    Update package script

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,5 +5,5 @@ yarn build
 yarn version --no-git-tag-version --no-commit-hooks
 cp README.md dist
 cp package.json dist
-git stash save --keep-index --include-untracked
+# git stash save --keep-index --include-untracked
 # np --yolo --no-yarn --contents=dist
