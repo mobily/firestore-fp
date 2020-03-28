b22a39f9c83a7b96ae4d0895414c24d8204f8d51
Marcin Dziewulski <hello@mobily.pl>
2020-03-28 13:36:57 +0100

    Point free style function

--- src/query.ts
+++ src/query.ts
@@ -3,6 +3,6 @@ import { pipe } from './pipe'
 
 export const query = (firestore: () => any): Piped => (...fns: any[]): any => {
   // @ts-ignore
-  const result = pipe(() => firestore(), ...fns)
+  const result = pipe(firestore, ...fns)
   return result()
 }
