9fc9654eae15978cc44fd3ca2f9e20fa7284587e
Marcin Dziewulski <hello@mobily.pl>
2020-03-28 01:32:26 +0100

    Fix collection types

--- src/collection.ts
+++ src/collection.ts
@@ -8,13 +8,15 @@ import { curry2 } from './internal/curry2'
 
 type Acc<T> = FirebaseFirestore | DocumentReference<T>
 
+type ExtractReference<T, X> = T extends infer U
+  ? CollectionReference<U>
+  : CollectionReference<X>
+
 type Curry2 = {
-  <T>(name: string): (acc: Acc<T>) => CollectionReference<T>
+  <T>(name: string): <X>(acc: Acc<X>) => ExtractReference<T, X>
   <T>(name: string, acc: Acc<T>): CollectionReference<T>
 }
 
-export const collection: Curry2 = curry2(
-  <T>(name: string, acc: Acc<T>): any => {
-    return acc.collection(name)
-  },
-)
+export const collection: Curry2 = curry2((name: any, acc: any) => {
+  return acc.collection(name)
+})
