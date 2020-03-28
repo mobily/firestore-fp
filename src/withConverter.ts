b3c651505396b7cc100d395d6aec90bf2046d4a1
Marcin Dziewulski <hello@mobily.pl>
2020-03-28 01:29:28 +0100

    Update withConverter types

--- src/withConverter.ts
+++ src/withConverter.ts
@@ -10,18 +10,31 @@ import { ExtractData } from './internal/types'
 
 type Acc = DocumentReference | CollectionReference | Query
 
-export type DataConverter<T> = {
-  fromFirestore: (data: T) => T
+type ExtractReference<T, X> = T extends CollectionReference
+  ? CollectionReference<X>
+  : T extends DocumentReference
+  ? DocumentReference<X>
+  : T extends Query
+  ? Query<X>
+  : never
+
+export type DataConverter<T, X> = {
+  fromFirestore: (data: T) => X
   toFirestore: (data: T) => DocumentData
 }
 
 type Curry2 = {
-  <T extends Acc>(converter: DataConverter<ExtractData<T>>): (acc: T) => T
-  <T>(converter: DataConverter<ExtractData<T>>, acc: T): T
+  <T extends Acc, X>(converter: DataConverter<ExtractData<T>, X>): (
+    acc: T,
+  ) => ExtractReference<T, X>
+  <T, X>(converter: DataConverter<ExtractData<T>, X>, acc: T): ExtractReference<
+    T,
+    X
+  >
 }
 
 export const withConverter: Curry2 = curry2(
-  (converter: DataConverter<any>, acc: any): any => {
+  (converter: DataConverter<any, any>, acc: any): any => {
     return acc.withConverter(converter)
   },
 )
