afeaa5822baa8d93b102e0950ebed46783549087
Marcin Dziewulski <hello@mobily.pl>
2020-03-29 20:12:23 +0200

    Update where type

--- src/where.ts
+++ src/where.ts
@@ -3,28 +3,39 @@ import {
   Query,
   FieldPath,
   WhereFilterOp,
+  DocumentData,
 } from '@firebase/firestore-types'
 
 import { curry4 } from './internal/curry4'
+import { ExtractData } from './internal/types'
 
 type Acc<T> = Query<T> | CollectionReference<T>
+type ValueByOperator<O, T> = O extends 'array-contains-any' | 'in' ? T[] : T
+type ExtractField<T> = Extract<keyof T, string>
+type ExtractValue<T, O, X> = T extends keyof X
+  ? ValueByOperator<O, X[T]>
+  : never
 
 type Curry4 = {
-  <T>(fieldPath: Extract<keyof T, string>): (
-    opStr: WhereFilterOp,
-  ) => <K extends keyof T>(value: T[K]) => <X>(acc: Acc<X & T>) => Query<X>
-  <T>(fieldPath: Extract<keyof T, string>, opStr: WhereFilterOp): <
-    K extends keyof T
+  <T>(fieldPath: ExtractField<T>): <O extends WhereFilterOp>(
+    opStr: O,
+  ) => <X extends DocumentData>(
+    value: ExtractValue<ExtractField<T>, O, X>,
+  ) => <Z extends Acc<X>>(acc: Z) => Query<ExtractData<Z>>
+  <T, O extends WhereFilterOp>(fieldPath: ExtractField<T>, opStr: O): <
+    X extends DocumentData
   >(
-    value: T[K],
-  ) => <X>(acc: Acc<X & T>) => Query<X>
-  <T, K extends keyof T>(fieldPath: K, opStr: WhereFilterOp, value: T[K]): (
-    acc: Acc<T>,
-  ) => Query<T>
-  <T, K extends keyof T>(
+    value: ExtractValue<ExtractField<T>, O, X>,
+  ) => <Z extends Acc<X>>(acc: Z) => Query<ExtractData<Z>>
+  <T, K extends keyof T, O extends WhereFilterOp>(
+    fieldPath: K,
+    opStr: O,
+    value: ValueByOperator<O, T[K]>,
+  ): (acc: Acc<T>) => Query<T>
+  <T, K extends keyof T, O extends WhereFilterOp>(
     fieldPath: T,
-    opStr: WhereFilterOp,
-    value: T[K],
+    opStr: O,
+    value: ValueByOperator<O, T[K]>,
     acc: Acc<T>,
   ): Query<T>
 }
